ad5a86fa8d3b166dabd3dddd0d42f5890476734c
MonicaBartha <barthamonica@gmail.com>
2019-12-03 16:06:02 -0300

    codigo monica primer pull request

--- src/data.js
+++ src/data.js
@@ -3,5 +3,6 @@
 // esta es una función de ejemplo
 
 export const example = () => {
+
   return 'example';
 };
--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@
  * import LoL from './data/lol/lol.js'
  * import POTTER from './data/potter/potter.js'
  */
+import POKEMON from './data/pokemon/pokemon.js'
 
-import { example } from './data.js';
+console.log(POKEMON);
 
-console.log(example);
 
 /*
  * console.log(POKEMON);
