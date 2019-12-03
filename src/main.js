de7db91b7e361c0318c69e259e5419cfcb52deb3
karmina-jc <karmina.jara@ug.uchile.cl>
2019-12-03 15:51:36 -0300

    flexbox css, espacios de despliegue html

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@
  * import POTTER from './data/potter/potter.js'
  */
 
-import { example } from './data.js';
+import POKEMON from './data/pokemon/pokemon.js';
 
-console.log(example);
+console.log(POKEMON);
 
 /*
  * console.log(POKEMON);
