b7a406a40abdbc2c2fadd13d1806959167079bf7
MonicaBartha <barthamonica@gmail.com>
2019-12-16 12:33:30 -0300

    CSS, cambie el estilo del overlay

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import { filtradoWeakness } from './data.js';
 import { sortBy } from './data.js';
 
 /* muestra los objetos */
-//const section = document.getElementById("todos"); 
 const main = document.getElementById('all')
 const overlay = document.getElementById('overlay');
 const fortaleza = ['Ground', 'Rock', 'water']
